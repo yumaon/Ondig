d4658d394a84d98b3f2c287d854a9228d6f517dc
yumaon <110388364+yumaon@users.noreply.github.com>
2022-12-11 13:58:07 +0000

    [Add]deviseのモデル、コントローラー、viewファイルの作成(public,artist,admin)

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
+  devise_for :admins
+  devise_for :artist_users
+  devise_for :public_users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
