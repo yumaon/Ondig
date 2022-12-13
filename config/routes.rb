2f7892037538f67ced6ba6f37e7b862b3b403598
yumaon <110388364+yumaon@users.noreply.github.com>
2022-12-13 00:27:48 +0000

    [Add]deviseのルーティングの編集 管理者用、一般会員用、アーティスト会員用のルーティングを設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,19 @@
 Rails.application.routes.draw do
-  devise_for :admins
-  devise_for :artist_users
-  devise_for :public_users
+  # 管理者用
+  devise_for :admin,skip: [:registrations, :passwords], controllers: {
+    sessions: "admin/sessions"
+  }
+
+  # アーティスト会員用
+  devise_for :artist_users,skip: [:passwords], controllers: {
+    registrations: "artist/registrations",
+    sessions: "artist/sessions"
+  }
+
+  # 一般会員用
+  devise_for :public_users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
