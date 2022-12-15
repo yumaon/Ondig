cc2832b75ce5b0cff34a8872a66655c4539f7c84
yumaon <110388364+yumaon@users.noreply.github.com>
2022-12-15 11:39:39 +0000

    [Add]一般会員側ログイン画面の実装 artist_userのregistrations_controller.rbにも一部コメントを追加

--- app/controllers/artist/registrations_controller.rb
+++ app/controllers/artist/registrations_controller.rb
@@ -40,7 +40,7 @@ class Artist::RegistrationsController < Devise::RegistrationsController
   # end
 
   protected
-
+  # sign up時に入力されたデータの保存を許可するための記述
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:artist_name, :rep_name, :rep_name_kana, :activity_location])
   end
--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -3,6 +3,7 @@
 class Public::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   # GET /resource/sign_up
   # def new
@@ -38,7 +39,11 @@ class Public::RegistrationsController < Devise::RegistrationsController
   #   super
   # end
 
-  # protected
+  protected
+  # sign up時に入力されたデータの保存を許可するための記述
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name, :last_name_kana, :first_name, :first_name_kana, :nickname])
+  end
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
