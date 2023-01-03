6680e1c334d31e19340bf5b33ee9a82fefbc45ca
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-03 03:31:14 +0000

    [Update]Artist会員 Genreを新しくcreateした場合にそのままupdateさせるよう記述を編集

--- app/controllers/artist/genres_controller.rb
+++ app/controllers/artist/genres_controller.rb
@@ -7,24 +7,27 @@ class Artist::GenresController < ApplicationController
 
   def create
     @genre = Genre.new(genre_params)
-    @genre.save
-    redirect_to artist_genres_path
+    if @genre.save
+      current_artist_user.genre_id = @genre.id
+      current_artist_user.save
+    end
+    redirect_to my_page_artist_artist_users_path
   end
-  
-  def update
-    @artist_user = current_artist_user
-    @artist_user.update(artist_user_params)
-    redirect_to artist_artist_users_my_page_path
-  end
+
+  # def update
+  #   @artist_user = current_artist_user
+  #   @artist_user.update(artist_user_params)
+  #   # redirect_to artist_artist_users_my_page_path
+  # end
 
   private
 
   def genre_params
     params.require(:genre).permit(:name)
   end
-  
-  def artist_user_params
-    params.require(:artist_user).permit(:genre_id)
-  end
+
+  # def artist_user_params
+  #   params.require(:artist_user).permit(:genre_id)
+  # end
 
 end
