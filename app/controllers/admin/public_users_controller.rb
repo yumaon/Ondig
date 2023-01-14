aedded4d653b66476c55cad2a979e4d518267e01
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-14 01:15:25 +0000

    [Add]管理者側　一般会員情報編集機能の実装

--- app/controllers/admin/public_users_controller.rb
+++ app/controllers/admin/public_users_controller.rb
@@ -8,5 +8,18 @@ class Admin::PublicUsersController < ApplicationController
   end
 
   def edit
+    @public_user = PublicUser.find(params[:id])
+  end
+
+  def update
+    @public_user = PublicUser.find(params[:id])
+    @public_user.update(public_user_params)
+    redirect_to admin_public_user_path
+  end
+
+  private
+
+  def public_user_params
+    params.require(:public_user).permit(:last_name, :first_name, :last_name_kana, :first_name_kana, :nickname, :email, :is_deleted)
   end
 end
