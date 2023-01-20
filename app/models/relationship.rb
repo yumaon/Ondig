4c9a903e7d3b31a8a03f7b635bb46e4572334a01
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-20 09:20:26 +0000

    [Update]Artist会員,一般会員 フォロー中とフォロワーで退会しているユーザーを非表示に設定

--- app/models/relationship.rb
+++ app/models/relationship.rb
@@ -3,4 +3,22 @@ class Relationship < ApplicationRecord
   belongs_to :artist_followed, class_name: "ArtistUser", optional: true
   belongs_to :public_follower, class_name: "PublicUser", optional: true
   belongs_to :public_followed, class_name: "PublicUser", optional: true
+
+  # フォローしている、退会していないユーザーのみを取り出す
+  scope :active_artist_followed, -> { where(artist_followed_id: ArtistUser.active) }
+  scope :active_public_followed, -> { where(public_followed_id: PublicUser.active) }
+
+  # フォロワーの、退会していないユーザーのみを取り出す
+  scope :active_artist_follower, -> { where(artist_follower_id: ArtistUser.active) }
+  scope :active_public_follower, -> { where(public_follower_id: PublicUser.active) }
+
+  # フォローしている、退会していないArtist会員と一般会員の数の合計値をだすメソッド
+  def self.following_count
+    active_artist_followed.count + active_public_followed.count
+  end
+
+  # フォローされている、退会していないArtist会員と一般会員の数の合計値を出すメソッド
+  def self.follower_count
+    active_artist_follower.count + active_public_follower.count
+  end
 end
