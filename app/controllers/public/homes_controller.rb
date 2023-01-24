3128d550bc37d3213da78237b50e04a673785ea7
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-24 02:45:30 +0000

    [Add]Topページの実装 [Update]ログイン無しで閲覧できるよう各々のviewページの記述を一部修正 [Update]レイアウト修正

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -1,6 +1,9 @@
 class Public::HomesController < ApplicationController
   def top
+    # 最新のArtistUserの情報を３件取得
     @artist_users = ArtistUser.active.order(created_at: :desc).limit(3)
+    # 最新のトピックスの情報を３件取得
+    @topics = Topic.active_topics.order(created_at: :desc).limit(3)
   end
 
   def about
--- app/controllers/public/profiles_controller.rb
+++ app/controllers/public/profiles_controller.rb
@@ -3,20 +3,22 @@ class Public::ProfilesController < ApplicationController
     @artist_user = ArtistUser.find(params[:id])
     @topic = Topic.order(created_at: :desc).find_by(artist_user_id: @artist_user.id)
 
-    @current_PublicUser_join = Join.where(public_user_id: current_public_user.id)
-    @ArtistUser_join = Join.where(artist_user_id: @artist_user.id)
+    if public_user_signed_in?
+      @current_PublicUser_join = Join.where(public_user_id: current_public_user.id)
+      @ArtistUser_join = Join.where(artist_user_id: @artist_user.id)
 
-    @current_PublicUser_join.each do |current_public|
-      @ArtistUser_join.each do |other_artist|
-        if current_public.room_id == other_artist.room_id then
-          @isRoom = true
-          @roomId = current_public.room_id
+      @current_PublicUser_join.each do |current_public|
+        @ArtistUser_join.each do |other_artist|
+          if current_public.room_id == other_artist.room_id then
+            @isRoom = true
+            @roomId = current_public.room_id
+          end
         end
-      end
-      if @isRoom
-      else
-        @room = Room.new
-        @join = Join.new
+        if @isRoom
+        else
+          @room = Room.new
+          @join = Join.new
+        end
       end
     end
   end
