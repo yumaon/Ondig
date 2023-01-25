1d485882d60677c6cadd1dea5f2747b35b59ef52
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-25 02:45:06 +0000

    [Update]Artist会員 アイテム登録、空欄での登録を無効

--- app/models/item.rb
+++ app/models/item.rb
@@ -2,4 +2,9 @@ class Item < ApplicationRecord
   belongs_to :artist_user
 
   has_one_attached :item_image
+
+  validates :name, presence: true
+  validates :introduction, presence: true
+  validates :price, presence: true
+  validates :item_image, presence: true
 end
