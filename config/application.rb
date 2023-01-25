49599f8b108d541271c8bf91cda76bea557feeaf
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-25 10:00:05 +0000

    [Update]タイムゾーンを日本時間に変更

--- config/application.rb
+++ config/application.rb
@@ -13,6 +13,8 @@ module Ondig
 
     config.i18n.default_locale = :ja
 
+    config.time_zone = 'Asia/Tokyo'
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
