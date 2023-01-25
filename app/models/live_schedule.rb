755ea778adb2fe56917c2ba64ce6882c86e1cf07
yumaon <110388364+yumaon@users.noreply.github.com>
2023-01-25 02:40:33 +0000

    [Update]Artist会員 Liveスケジュール、空欄での登録を無効

--- app/models/live_schedule.rb
+++ app/models/live_schedule.rb
@@ -1,3 +1,8 @@
 class LiveSchedule < ApplicationRecord
   belongs_to :artist_user
+
+  validates :name, presence: true
+  validates :schedule_date, presence: true
+  validates :price, presence: true
+  validates :location, presence: true
 end
