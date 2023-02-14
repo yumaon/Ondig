9215cde71e583aad167b83b0124308dd8193aa10
yumaon <110388364+yumaon@users.noreply.github.com>
2023-02-14 04:01:13 +0000

    [Update]Topページ PopularArtistの表示を追加

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -2,6 +2,8 @@ class Public::HomesController < ApplicationController
   def top
     # 最新のArtistUserの情報を３件取得
     @artist_users = ArtistUser.active.order(created_at: :desc).limit(3)
+
+    @popular_artist_users = ArtistUser.active.sort {|a, b| b.active_followers.count.to_i <=> a.active_followers.count.to_i}.slice(0..2)
     # 最新のトピックスの情報を３件取得
     @topics = Topic.active_topics.order(created_at: :desc).limit(3)
   end
