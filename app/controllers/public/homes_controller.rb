718e5424950f093377f5fe99041e6d3bf2c7527c
yumaon <110388364+yumaon@users.noreply.github.com>
2023-02-14 21:55:14 +0000

    [Update]Topページ いいね数が多いトピックスを３件表示

--- app/controllers/public/homes_controller.rb
+++ app/controllers/public/homes_controller.rb
@@ -3,9 +3,14 @@ class Public::HomesController < ApplicationController
     # 最新のArtistUserの情報を３件取得
     @artist_users = ArtistUser.active.order(created_at: :desc).limit(3)
 
+    # フォロワーが多いArtistUserを３件取得
     @popular_artist_users = ArtistUser.active.sort {|a, b| b.active_followers.count.to_i <=> a.active_followers.count.to_i}.slice(0..2)
+
     # 最新のトピックスの情報を３件取得
     @topics = Topic.active_topics.order(created_at: :desc).limit(3)
+
+    # いいねの数が多いトピックスを3件取得
+    @popular_topics = Topic.active_topics.sort {|a, b| b.favorites.count.to_i <=> a.favorites.count.to_i}.slice(0..2)
   end
 
   def about
