91bd7e8b74ce2a37339d4cb5fcbf860b7e600cba
yumaon <110388364+yumaon@users.noreply.github.com>
2023-02-20 00:15:05 +0000

    [Update]Artist会員側 他ユーザーのDMルーム一覧画面へ遷移できないよう設定

--- app/controllers/artist/rooms_controller.rb
+++ app/controllers/artist/rooms_controller.rb
@@ -11,27 +11,32 @@ class Artist::RoomsController < ApplicationController
   # ダイレクトメッセージのやり取りしているRoomの一覧
   def index
     @artist_user = ArtistUser.find(params[:artist_user_id])
-    # 取得したArtist会員の参加しているroom.idをmy_room_idに格納
-    my_room_id = []
-    @artist_user.joins.each do |join|
-      my_room_id << join.room.id
-    end
 
-    # 自分が参加している、roomのjoinテーブルを取得
-    my_joins = Join.where(room_id: my_room_id)
+    if @artist_user == current_artist_user
+      # 取得したArtist会員の参加しているroom.idをmy_room_idに格納
+      my_room_id = []
+      @artist_user.joins.each do |join|
+        my_room_id << join.room.id
+      end
 
-    # 自分のidが含まれないjoinテーブルを取り出す。
-    another_join_id_box = []
-    my_joins.each do |join|
-      if join.artist_user_id.present?
-        unless join.artist_user_id == current_artist_user.id
+      # 自分が参加している、roomのjoinテーブルを取得
+      my_joins = Join.where(room_id: my_room_id)
+
+      # 自分のidが含まれないjoinテーブルを取り出す。
+      another_join_id_box = []
+      my_joins.each do |join|
+        if join.artist_user_id.present?
+          unless join.artist_user_id == current_artist_user.id
+            another_join_id_box << join.id
+          end
+        elsif join.public_user_id.present?
           another_join_id_box << join.id
         end
-      elsif join.public_user_id.present?
-        another_join_id_box << join.id
       end
+      @another_joins = Join.where(id: another_join_id_box).order(created_at: :desc).page(params[:page]).per(8)
+    else
+      redirect_to artist_artist_user_rooms_path(current_artist_user)
     end
-    @another_joins = Join.where(id: another_join_id_box).order(created_at: :desc).page(params[:page]).per(8)
   end
 
   # ダイレクトメッセージRoomの詳細画面
