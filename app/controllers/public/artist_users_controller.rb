6a06b0d6cdacae9319d7310c0215a2dc50f643f8
yumaon <110388364+yumaon@users.noreply.github.com>
2023-02-22 05:29:49 +0000

    [Add]Artist一覧 絞り込み検索機能に表示順指定機能の追加

--- app/controllers/public/artist_users_controller.rb
+++ app/controllers/public/artist_users_controller.rb
@@ -20,7 +20,19 @@ class Public::ArtistUsersController < ApplicationController
     @public_user = current_public_user
     @genres = Genre.all
     @search_params = artist_user_search_params
-    @artist_users = ArtistUser.active.search(@search_params).includes(:genre).order(created_at: :desc).page(params[:page]).per(9)
+    # 最新順でArtistUserを表示させる場合
+    if params[:display_select] == "1"
+      @artist_users = ArtistUser.active.search(@search_params).includes(:genre).order(created_at: :desc).page(params[:page]).per(9)
+    # 登録日順でArtistUserを表示させる場合
+    elsif params[:display_select] == "2"
+      @artist_users = ArtistUser.active.search(@search_params).includes(:genre).page(params[:page]).per(9)
+    # フォロワーが多い順でArtistUserを表示させる場合
+    elsif params[:display_select] == "3"
+      @artist_users = Kaminari.paginate_array(ArtistUser.active.search(@search_params).includes(:genre).sort {|a, b| b.active_followers.count.to_i <=> a.active_followers.count.to_i}).page(params[:page]).per(9)
+    # フォロワーが少ない順でArtistUserを表示させる場合
+    elsif params[:display_select] == "4"
+      @artist_users = Kaminari.paginate_array(ArtistUser.active.search(@search_params).includes(:genre).sort {|a, b| a.active_followers.count.to_i <=> b.active_followers.count.to_i}).page(params[:page]).per(9)
+    end
     render "index"
   end
 
